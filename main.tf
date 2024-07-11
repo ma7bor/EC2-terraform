8cbe00226dfe3ff89bd64fc1c52a58598ce17995
Marwan <achbari.marouane@gmail.com>
2024-07-11 23:26:34 +0100

    remove access key

--- main.tf
+++ main.tf
@@ -1,47 +1,47 @@
 
 provider "aws" {
-    region = var.AWS_REGION
-    access_key = var.AWS_ACCESS_KEY
-    secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 
 }
 
 variable "AWS_ACCESS_KEY" {
-//default = "AKIA2VNT4SWG6TGGKJUN"
-default = "your access key here"
+  default = "your access key here"
 }
+
 variable "AWS_SECRET_KEY" {
-    default = "your secret key here"
+  default = "your secret key here"
 }
 
 variable "AWS_REGION" {
-    default = "your region here"
+  default = "your region here"
 }
 
 resource "aws_security_group" "instance_security" {
-    name = "terraform-test"
+  name = "terraform-test"
 
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "my_ec2_instance" {
-   ami           = "ami-005e54dee72cc1d00" # us-west-2
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance_security.id]
+  ami                    = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance_security.id]
 
-	user_data = <<-EOF
+  user_data = <<-EOF
 		#!/bin/bash
         sudo apt-get update
 		sudo apt-get install -y apache2
@@ -49,12 +49,12 @@ resource "aws_instance" "my_ec2_instance" {
 		sudo systemctl enable apache2
 		sudo echo "<h1>Hello devopssec</h1>" > /var/www/html/index.html
 	EOF
-    
-    tags = {
-        Name = "terraform test"
-    }
+
+  tags = {
+    Name = "terraform test"
+  }
 }
 
 output "public_ip" {
-    value = aws_instance.my_ec2_instance.public_ip
+  value = aws_instance.my_ec2_instance.public_ip
 }
